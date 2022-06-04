99d8e3c56f579fecca8e9c9987149b313cacc69d
feldjoshuanoah <feld.joshuanoah@gmail.com>
2022-06-04 15:29:45 +0200

    Add a function for calculating the element matrix

--- src/Truss.cpp
+++ src/Truss.cpp
@@ -2,7 +2,29 @@
 
 Truss::Truss(Node first, Node second): first{first}, second{second} {}
 
+Node Truss::getFirst()
+{
+	return first;
+}
+
+Node Truss::getSecond()
+{
+	return second;
+}
+
 float Truss::length()
 {
 	return first.distance(second);
+}
+
+std::vector<std::vector<float>> Truss::getElementMatrix()
+{
+	float l_s = (first.getX() - second.getX()) / length();
+	float m_s = (first.getY() - second.getY()) / length(); 
+	return {
+		{l_s * l_s, l_s * m_s, -(l_s * l_s), -l_s * m_s},
+		{l_s * m_s, m_s * m_s, -l_s * m_s, -(m_s * m_s)},
+		{-(l_s * l_s), -l_s * m_s, l_s * l_s, l_s * m_s},
+		{-l_s * m_s, -(m_s * m_s), l_s * m_s, m_s * m_s}
+	};;
 }
--- src/Truss.h
+++ src/Truss.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Node.h"
+#include <vector>
 
 /**
  * Represents a truss connecting two nodes in a straight line.
@@ -26,9 +27,44 @@ public:
 	Truss(Node first, Node second);
 
 	/**
+	 * Gets the first node connected to this truss.
+	 * 
+	 * @returns The first connected node.
+	 */
+	Node getFirst();
+
+	/**
+	 * Sets the first node connected to this truss.
+	 * 
+	 * @param first The first connected node.
+	 */
+	void setFirst(Node first);
+
+	/**
+	 * Gets the second node connected to this truss.
+	 * 
+	 * @returns The second connected node.
+	 */
+	Node getSecond();
+
+	/**
+	 * Sets the second node connected to this truss.
+	 * 
+	 * @param second The second connected node.
+	 */
+	void setSecond(Node second);
+
+	/**
 	 * Gets the length of this truss (distance between the two nodes).
 	 * 
 	 * @returns The length.
 	 */
 	float length();
+
+	/**
+	 * Gets the element matrix of this truss.
+	 * 
+	 * @returns The element matrix.
+	 */
+	std::vector<std::vector<float>> getElementMatrix();
 };
